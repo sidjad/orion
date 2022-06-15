875f8cac4c6d14a5695ca4e295dd321264b87330
siddhesh <siddheshj30@gmail.com>
2022-06-15 11:53:58 +0530

    Nmap Class updated

--- nmap_cl.py
+++ nmap_cl.py
@@ -11,21 +11,26 @@ nmap_type = input("Enter Here the type: ")
 class nmap():
     def __init__(self,hostname,**args):
         self.hostname=hostname
-        self.hostname_nmap = hostname + "_nmap.xml"
+        self.hostname_nmap_basic = hostname + "_nmap_basic.xml"
+        self.hostname_nmap_medium = hostname + "_nmap_medium.xml"
+        self.hostname_nmap_advance = hostname + "_nmap_advance.xml"
     def nmap_basic(self):
-        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T4 {0} -oX {1}".format(self.hostname,self.hostname_nmap),stdout=subprocess.PIPE,shell=True)
+        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T4 {0} -oX {1}".format(self.hostname,self.hostname_nmap_basic),stdout=subprocess.PIPE,shell=True)
 
     def nmap_medium(self):
-        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T4  --script vuln {0} -oX {1}".format(self.hostname,self.hostname_nmap),stdout=subprocess.PIPE,shell=True)
+        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T4  --script vuln {0} -oX {1}".format(self.hostname,self.hostname_nmap_medium),stdout=subprocess.PIPE,shell=True)
 
     def nmap_advance(self):
-        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T3 -Pn --script vuln -p- {0} -oX {1}".format(self.hostname,self.hostname_nmap),stdout=subprocess.PIPE,shell=True)
+        self.nmap_opt_one_out = subprocess.run("nmap -sV -sC -T3 -Pn --script vuln -p- {0} -oX {1}".format(self.hostname,self.hostname_nmap_advance),stdout=subprocess.PIPE,shell=True)
 
 
 
+"""
+Calling object within if else condition for switching object acording to user nmap parameter
 
+"""
 
-obj1=nmap(hostname)
+obj1=nmap(hostname)  #main Object of nmap class
 if nmap_type == "b":
     obj1.nmap_basic()
 elif nmap_type == "m":
